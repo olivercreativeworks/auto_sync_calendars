c3463d82c947859cddfd76018214456104485e76
Oliver <olivercreativeworks@gmail.com>
2024-07-26 12:14:00 -0400

    Refactor Code.js and rename to syncCalendars.js

---
+++ syncCalendars.js
@@ -0,0 +1,138 @@
+/**
+ * Updates the calendar with the targetCalendarId with the events from the sourceCalendarId.
+ * @param {string} targetCalendarId
+ * @param {string} sourceCalendarId
+ */
+function syncCalendars(sourceCalendarId, targetCalendarId){
+  const tokenManager = getTokenManager()
+  do{
+    const sourceEvents = getCalendarEvents(
+      sourceCalendarId, 
+      tokenManager.getSavedPageToken(), 
+      tokenManager.getSavedSyncToken()
+    )
+
+    sourceEvents.items.forEach(
+      sourceEvent => syncCalendarWithSourceEvent(targetCalendarId, sourceEvent)
+    )
+
+    tokenManager.updateTokens(sourceEvents.nextPageToken, sourceEvents.nextSyncToken)
+  } while(tokenManager.hasPageToken())
+  
+  return
+
+  function getTokenManager(){
+    const syncTokenSymbol = 'syncToken'
+    const pageTokenSymbol = 'pageToken'
+    const props = PropertiesService.getScriptProperties() 
+    return {
+      getSavedSyncToken: () => props.getProperty(syncTokenSymbol),
+      getSavedPageToken: () => props.getProperty(pageTokenSymbol),
+      hasPageToken: () => !!(props.getProperty(pageTokenSymbol)),
+      /** @param {string} [pageToken] @param {string} [syncToken] */
+      updateTokens: (pageToken, syncToken) => {
+        pageToken ? props.setProperty(pageTokenSymbol, pageToken) : props.deleteProperty(pageTokenSymbol)
+        syncToken && props.setProperty(syncTokenSymbol, syncToken)
+      }
+    }
+  }
+
+  /**
+   * @param {string} calendarId
+   * @param {string} [pageToken]
+   * @param {string} [syncToken]
+   */
+  function getCalendarEvents(calendarId, pageToken, syncToken){
+    return Calendar.Events.list(calendarId, {syncToken, pageToken})
+  }
+
+  /**
+   * @param {string} calendarId
+   * @param {Calendar_v3.Calendar.V3.Schema.Event} sourceEvent
+   */
+  function syncCalendarWithSourceEvent(calendarId, sourceEvent){
+    // The event in the calendar should have the same id as the source event
+    const existingEvent = getEvent(calendarId, sourceEvent.id)
+    if(!existingEvent) {
+      return createEvent(sourceEvent, calendarId)
+    }
+    else if(noUpdatesRequired(sourceEvent, existingEvent)){
+      return
+    }
+    else if(isCancelled(sourceEvent)){
+      return removeEvent(existingEvent.id, calendarId)
+    }
+    else{
+      // The sequence must match the existing event's sequence to avoid an error
+      return updateEvent(existingEvent.id, calendarId, {...sourceEvent, sequence:existingEvent.sequence})
+    }    
+  }
+
+  /**
+   * @param {string} calendarId
+   * @param {string} eventId
+   */
+  function getEvent(calendarId, eventId){
+    try{
+      return Calendar.Events.get(calendarId, eventId)
+    }catch(err){
+      if(notFoundError(err)) {
+        return undefined
+      }else{
+        throw err
+      }
+    }
+  }
+
+  /**
+   * @param {Partial<{details:{message:string, code:number}}>} err
+   */
+  function notFoundError(err){
+    return err.details?.message.match('Not Found') && err.details?.code === 404
+  }
+
+  /**
+   * @param {Calendar_v3.Calendar.V3.Schema.Event} eventResource
+   * @param {string} calendarId
+   */
+  function createEvent(eventResource, calendarId){
+    console.log(`Creating event from: ${JSON.stringify(eventResource)} on calendar with id ${calendarId}`)
+    return Calendar.Events.insert(eventResource, calendarId)
+  }
+
+  /**
+   * @param {Calendar_v3.Calendar.V3.Schema.Event} sourceEvent
+   * @param {Calendar_v3.Calendar.V3.Schema.Event} targetEvent
+   */
+  function noUpdatesRequired(sourceEvent, targetEvent){
+    return isCancelled(sourceEvent) && isCancelled(targetEvent)
+  }
+
+  /**
+   * @param {Calendar_v3.Calendar.V3.Schema.Event} event
+   */
+  function isCancelled(event){
+    return event.status === "cancelled"
+  }
+
+  /**
+   * @param {string} eventId
+   * @param {string} calendarId
+   */
+  function removeEvent(eventId, calendarId){
+    console.log(`Deleting event with id: ${eventId} on calendar with id: ${calendarId}`)
+    Calendar.Events.remove(calendarId, eventId)
+  }
+
+  /**
+   * @param {string} eventId
+   * @param {string} calendarId
+   * @param {Calendar_v3.Calendar.V3.Schema.Event} eventResource
+   */
+  function updateEvent(eventId, calendarId, eventResource){
+    console.log(`Updating event with id ${eventId} on calendar with id ${calendarId}.`)
+    return Calendar.Events.update(eventResource, calendarId, eventId)
+  }
+}
+
+
