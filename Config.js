2c3e044303cfc3a109b8e44b409e6fb93932ef3b
Oliver <olivercreativeworks@gmail.com>
2024-07-26 15:05:04 -0400

    Add instructions on how to populate config

--- Config.js
+++ Config.js
@@ -1,7 +1,27 @@
-const manager = getCalendarIdManager()
+const calendarManager = getCalendarIdManager()
+const scriptUrlManager = getScriptUrlManager()
+
+/** 
+ * Update the config object below. The following tips might be helpful:
+ * - The source calendar is the calendar you want to copy events from. The target is the calendar you will copy events to. 
+ *    - You can find your calendar ids via the Calendar.CalendarList.list method. See:https://developers.google.com/calendar/api/v3/reference/calendarList/list
+ * - You will need to deploy this script as a webapp and enter the url into the config object's scriptUrl property. 
+ *    - Here are instructions on how to deploy the script so you can get the url: https://developers.google.com/apps-script/guides/web#deploy_a_script_as_a_web_app. 
+ *    - The url should look similar to this, where YOUR-UNIQUE-ID is a random collection of characters: 'https://script.google.com/macros/s/YOUR-UNIQUE-ID/exec'
+*/
 const CONFIG = {
-  sourceCalendarId: manager.getSourceCalendarId(), //Enter the id of the calendar you want to copy events from
-  targetCalendarId: manager.getTargetCalendarId()  //Enter the id of the calendar you want to copy events to
+  sourceCalendarId: calendarManager.getSourceCalendarId(),
+  targetCalendarId: calendarManager.getTargetCalendarId(),
+  scriptUrl: scriptUrlManager.getScriptUrl() 
+}
+
+function getScriptUrlManager(){
+  const urlSymbol = 'url'
+  const props = PropertiesService.getScriptProperties()
+  return {
+    getScriptUrl:() => props.getProperty(urlSymbol),
+    setScriptUrl:(url) => props.setProperty(urlSymbol, url)
+  }
 }
 
 function getCalendarIdManager(){
