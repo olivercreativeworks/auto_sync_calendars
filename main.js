2a6418add94040ea0858e27bb752ea7d825f888b
Oliver <olivercreativeworks@gmail.com>
2024-07-26 21:17:17 -0400

    Update function to use new signature

--- main.js
+++ main.js
@@ -2,12 +2,12 @@
  * Run this to activate autosync. Update the config file to change the calendars you want to sync. 
  */
 function startAutoSync(){
-  autoSyncManager().activate()
+  autoSyncManager(CONFIG.scriptUrl).activate(CONFIG.sourceCalendarId)
 }
 
 /**
  * Run this to stop autosync.
  */
 function endAutoSync(){
-  autoSyncManager().disable()
+  autoSyncManager(CONFIG.scriptUrl).disable()
 }
