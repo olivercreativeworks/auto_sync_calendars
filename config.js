bc02e47eecbf1d38b13f74d317b5a7fda8869050
Oliver <olivercreativeworks@gmail.com>
2024-07-28 18:34:21 -0400

    Shorten instructions in config file. Made file name lowercase.

---
+++ config.js
@@ -0,0 +1,10 @@
+/**
+ * Source calendar id: the id of the calendar you want to copy events from. 
+ * Target calendar id: the id of the calendar you will copy events to. 
+ * Script url: the url of this web app. This is optional. See readme for more info.
+ */
+const CONFIG = {
+  sourceCalendarId:'',
+  targetCalendarId:'',
+  scriptUrlOptional:'' 
+}
