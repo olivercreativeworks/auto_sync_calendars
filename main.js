f82a7b1fbe295b2ecacb973154efa9012c512038
Oliver <olivercreativeworks@gmail.com>
2024-08-09 12:45:30 -0400

    simplify config setup by allowing user to provide the calendar name instead of the id

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
 /**
- * Start by updating the config in the function below. Then run the function to save any config updates.
- * Once you saved your config updates, you can run startAutoSync to start syncing your calendars.
- */
-function updateConfig(){
-  const CONFIG = {
-    sourceCalendarId:'',
-    targetCalendarId:'',
-    scriptUrlOptional:'' 
-  }
-  AutoSync.settings.update(CONFIG.sourceCalendarId, CONFIG.targetCalendarId, CONFIG.scriptUrlOptional)
-}
-
-/**
- * Start automatically syncing your calendars.
+ * Start by updating the config in the function below. 
+ * 
+ * Then run the function to start automatically syncing your calendars.
  */
 function startAutoSync(){
-  AutoSync.activate()
+  const CONFIG = {
+    sourceCalendarName:'buzz',
+    targetCalendarName:'woody',
+    scriptUrlOptional:'https://script.google.com/macros/s/AKfycbz2wKNGf1h31oDvgG_g8raNKVhD5BOD7yTeMalq9cYDoKgAlC7LPygBdbVuIGS3LJZJsw/exec' 
+  }
+  
+  const sourceCalendarId = getCalendarId(CONFIG.sourceCalendarName)
+  const targetCalendarId = getCalendarId(CONFIG.targetCalendarName)
+  AutoSync.start(sourceCalendarId, targetCalendarId, CONFIG.scriptUrlOptional)
+
+  function getCalendarId(calendarName){
+    const cal = CalendarApp.getCalendarsByName(calendarName)[0]
+    return cal.getId()
+  }
 }
 
 /**
