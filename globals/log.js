9a9baca38050056561ca35858cb89af4760081ab
Oliver <olivercreativeworks@gmail.com>
2024-08-09 14:55:02 -0400

    implemented global logging variable

---
+++ globals/log.js
@@ -0,0 +1,27 @@
+/** @type {(message:string|Error) => void} A global logging function */
+let log;
+updateGlobalLog(defaultLogger_)
+
+/**
+ * Updates the global variable, ```log```
+ * @template A
+ * @param {A} updatedLog
+ * @return A
+ */
+function updateGlobalLog(updatedLog){
+  log = updatedLog
+  return log
+}
+
+/** @param {string | Error} message */
+function defaultLogger_(message){
+  if(message instanceof Error){
+    console.warn(message)
+    console.log(message.name)
+    console.log(message.message)
+    console.log(message.stack)
+    message.details && console.log(message.details)
+  }else{
+    console.log(message)
+  }
+}
